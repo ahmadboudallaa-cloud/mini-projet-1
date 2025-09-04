64c3775aef2220dfeac90278d5132dccf8978cf1
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2025-09-04 10:54:23 +0200

    mini project 1

--- main.c
+++ main.c
@@ -29,28 +29,63 @@ int main()
 
     if(Moyenne >= 90)
         {
-      printf(" %.2f,Excellent .\n", Moyenne) ;
+      printf(" %.2f,Montion : Excellent .\n", Moyenne) ;
       }
       else if (Moyenne >= 80 && Moyenne <= 89){
-         printf("Tres bien.\n",Moyenne) ;
+         printf("Montion : Tres bien.\n",Moyenne) ;
 
       }
       else if (Moyenne >=70 && Moyenne <=79){
-          printf("Bien.\n",Moyenne) ;
+          printf("Montion : Bien.\n",Moyenne) ;
       }
       else if (Moyenne >= 60 && Moyenne <= 69){
-          printf(" Passable.\n",Moyenne) ;
+          printf(" Montion : Passable.\n",Moyenne) ;
       }
       else  {
-          printf(" Echec.\n",Moyenne) ;
+          printf(" Montion : Echec.\n",Moyenne) ;
            }
 if (Moyenne >= 50){
-    printf("réussi.\n", Moyenne);
+    printf("Ta Reussi.\n", Moyenne);
 }
 
-else ({
-    printf("echoe.\n", Moyenne);
+else {
+    printf("Ta Echoue.\n", Moyenne);
 }
+
+ float Max = Note1;
+
+ if(Note2 > Max){
+     Max = Note2;
+ }
+ if(Note3 > Max){
+     Max = Note3;
+ }
+if(Note4 > Max){
+    Max = Note4;
+}
+if(Note5 > Max){
+    Max = Note5;
+}
+ printf("La note la plus élevée est : %.2f\n", Max);
+ float Min = Note1;
+
+  if(Note2 < Min){
+      Min = Note2;
+  }
+
+  if(Note3 < Min){
+      Min = Note3;
+
+  }
+
+  if(Note4 < Min){
+      Min = Note4;
+  }
+  if(Note5 < Min){
+      Min = Note5;
+  }
+   printf("La note la plus bas est : %.2f\n", Min);
+
 
     return 0;
 }
